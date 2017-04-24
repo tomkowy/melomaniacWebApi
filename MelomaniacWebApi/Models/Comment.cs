bfc481605cdcf46cae2f2959310cf4573d89828b
Mateusz Chodkowski <chotkos@gmail.com>
2017-04-24 23:04:29 +0200

    minor changes

--- MelomaniacWebApi/Models/Comment.cs
+++ MelomaniacWebApi/Models/Comment.cs
@@ -2,10 +2,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Web;
 
 namespace MelomaniacWebApi.Models
-{
+{ 
     public class Comment
     {
         [BsonId]
