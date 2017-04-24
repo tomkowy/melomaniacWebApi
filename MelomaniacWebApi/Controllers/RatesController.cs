7f38fd6114990876e1057918a60c48f92fd3611e
pioszy <krauzig@gmail.com>
2017-04-25 00:21:42 +0200

    added method to get the average rate of track in RatesController

--- MelomaniacWebApi/Controllers/RatesController.cs
+++ MelomaniacWebApi/Controllers/RatesController.cs
@@ -11,7 +11,7 @@ namespace MelomaniacWebApi.Controllers
 {
     public class RatesController : ApiController
     {
-        //GET api/rates/GetAllTrackRatess/5
+        //GET api/rates/GetAllTrackRates/5
         [HttpGet]
         public IEnumerable<Rate> GetAllTrackRates(int data)
         {
@@ -19,6 +19,30 @@ namespace MelomaniacWebApi.Controllers
             var res = dbconn.GetTrackRates(data);
             return res;
         }
+
+        //GET api/rates/GetTrackAverageRate/5
+        [HttpGet]
+        public string GetTrackAverageRate(int data)
+        {
+            DBConnection dbconn = new DBConnection();
+            var res = dbconn.GetTrackRates(data);
+            int sum = 0;
+            foreach (var item in res)
+            {
+                sum += item.mark;
+            }
+            //można to policzyć w widoku
+            int numberOfElements = ((List<Rate>)res).Count;
+            double avg = sum / numberOfElements;
+
+            string jsonRes = "{'avg':'" + avg 
+                + "','sum':'" + sum 
+                + "','numberOfElements':" + numberOfElements
+                +"'}";
+
+            return jsonRes;
+        }
+
         //POST api/rates/EditRate/
         [HttpPost]
         public bool EditRate([FromBody]Rate data)
